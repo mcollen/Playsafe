018302ddd8edd8b995d2f21ea48e4eccd33ae763
mcollen <collen@wooddesigns.co.za>
2020-04-30 17:36:02 +0300

    web service conversions function complete

--- src/com/playsafe/conversions/ktoc.java
+++ src/com/playsafe/conversions/ktoc.java
@@ -1,5 +1,8 @@
 package com.playsafe.conversions;
 
+import javax.jws.WebService;
+
+@WebService(name = "ktoc", targetNamespace = "http://conversions.playsafe.com/")
 public interface ktoc {
 	
 	public double toCelsius(double kelvin);
